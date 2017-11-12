58ab3b9cee2fe9459e48ced58af64daf9e619020
pirlo-san <286378322@qq.com>
2017-11-12 10:32:13 +0800

    server templates

--- project/deploy.sh
+++ project/deploy.sh
@@ -3,4 +3,4 @@
 set -e -u -x
 make clean
 make all
-docker run -d -p 80:80 --name server server:latest
+docker run -d -p $1:$1 --name server server:latest
--- server/src/home/home.go
+++ server/src/home/home.go
@@ -6,7 +6,7 @@ import (
 )
 
 func HomePage(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("header.html", "footer.html")
+	t, _ := template.ParseFiles("/root/gopath/src/home/header.html", "/root/gopath/src/home/footer.html")
 	err := t.Execute(w, map[string]string{"Title": "My title", "Body": "Hi this is my body"})
 	if err != nil {
 		panic(err)
